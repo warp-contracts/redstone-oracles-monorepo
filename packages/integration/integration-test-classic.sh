0c932efba5269108fe729b1550d882054fbe1d98
robal <robert@warp.cc>
2023-06-13 14:06:55 +0200

    integration tests classic model (#592)
    * feat: integration tests for classic model
    
    * fix: calculate value deviation in relayer - no value stored in contract

--- packages/integration/integration-test-classic.sh
+++ packages/integration/integration-test-classic.sh
@@ -15,11 +15,16 @@ HARDHAT_MOCK_PRIVATE_KEY=ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7
 declare mongoDbPid
 declare cacheLayerPid
 declare oracleNodePid
+declare hardhatNodePid
+declare onChainRelayerPid
 
 stopAll() {
-  [ -n "$oracleNodePid" ] && kill -- -$oracleNodePid
-  [ -n "$cacheLayerPid" ] && kill -- -$cacheLayerPid
-  [ -n "$mongoDbPid" ] && kill -- -$mongoDbPid
+  set +e # ignore errors during cleanup, e.g. when process is already stopped
+  [ -n "$onChainRelayerPid" ] && echo "stop on-chain-relayer" && kill -- -$onChainRelayerPid
+  [ -n "$hardhatNodePid" ] && echo "stop hardhat node" && kill -- -$hardhatNodePid
+  [ -n "$oracleNodePid" ] && echo "stop oracle-node" && kill -- -$oracleNodePid
+  [ -n "$cacheLayerPid" ] && echo "stop cache-service" && kill -- -$cacheLayerPid
+  [ -n "$mongoDbPid" ] && echo "stop mongo" && kill -- -$mongoDbPid
 }
 
 trap 'stopAll' EXIT
@@ -69,12 +74,42 @@ main() {
   # Querying data packages from cache service
   curl http://localhost:3000/data-packages/latest/mock-data-service
 
-  # Using data in evm-connector
-  cd ../evm-connector
+  # Compile on-chain-relayer
+  cd ../on-chain-relayer
   lazilyInstallNPMDeps
   yarn compile
   lazilyBuildTypescript
-  runWithLogPrefix "yarn test test/monorepo-integration-tests/localhost-mock.test.ts" "evm-connector"
+
+  # Launch local EVM instance with hardhat
+  runWithLogPrefixInBackground "yarn start-node" "hardhat-node"
+  hardhatNodePid=$!
+  until curl 127.0.0.1:8545; do # wait for hardhat to start blockchain instance
+    sleep 1
+  done
+  yarn hardhat --network localhost run test/monorepo-integration-tests/scripts/deploy-mock-adapter.ts
+  ADAPTER_CONTRACT_ADDRESS="$(cat adapter-contract-address.txt)"
+  export ADAPTER_CONTRACT_ADDRESS
+
+  # Launch on-chain-relayer
+  cp .env.example .env
+  updateDotEnvFile "RPC_URL" "http://127.0.0.1:8545"
+  updateDotEnvFile "CHAIN_ID" "31337"
+  updateDotEnvFile "PRIVATE_KEY" "$HARDHAT_MOCK_PRIVATE_KEY"
+  updateDotEnvFile "ADAPTER_CONTRACT_ADDRESS" "$ADAPTER_CONTRACT_ADDRESS"
+  updateDotEnvFile "DATA_FEEDS" '["BTC", "ETH", "AAVE"]'
+  updateDotEnvFile "DATA_SERVICE_ID" "mock-data-service"
+  updateDotEnvFile "CACHE_SERVICE_URLS" '["http://localhost:3000"]'
+  updateDotEnvFile "HEALTHCHECK_PING_URL" ""
+  cat .env
+  runWithLogPrefixInBackground "yarn start" "on-chain-relayer"
+  onChainRelayerPid=$!
+
+  # Verify prices on-chain
+  until runWithLogPrefix "yarn hardhat --network localhost run test/monorepo-integration-tests/scripts/verify-mock-prices.ts" "relayer-contract"
+  do
+    # wait for relayer to put data on-chain
+    sleep 5
+  done
 
   # Cleanup
   trap -- EXIT
