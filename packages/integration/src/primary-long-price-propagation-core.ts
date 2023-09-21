7afb159049a689af9a9255342556049cf34a0578
robal <robert@warp.cc>
2023-09-21 10:52:19 +0200

    fix: increase cold start to 4 iterations for primary manifest long run (#1138)

--- packages/integration/src/primary-long-price-propagation-core.ts
+++ packages/integration/src/primary-long-price-propagation-core.ts
@@ -8,7 +8,7 @@ const DATA_FEEDS_NOT_WORKING_LOCALLY = ["PREMIA-TWAP-60"];
   const manifestFileName = "data-services/primary";
   const nodeWorkingTimeInMinutes = 3;
   const nodeIntervalInMilliseconds = 10000;
-  const coldStartIterationsCount = 3;
+  const coldStartIterationsCount = 4;
   await runLongPricePropagationCoreTest(
     manifestFileName,
     nodeWorkingTimeInMinutes,
